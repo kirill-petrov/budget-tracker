04234da83f3c2e2f355a871d99061dbeb39f133b
kirill petrov <fantaskiras@gmail.com>
2022-04-17 23:54:02 +0300

    fix combineReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import transactionsReducer from './reducers/transactionsReducer';
-import todoReducer from './reducers/todoReducer';
 
 const rootReducer = combineReducers({
   transactions: transactionsReducer,
-  todo: todoReducer,
 });
 
 const store = configureStore({
